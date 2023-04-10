9b0e6027b1838572ddb967213fd1926a8ef3ee93
wcheron <wcheron@univ-rennes1.fr>
2023-04-10 13:54:59 +0200

    header animation

---
+++ js/script.js
@@ -0,0 +1,9 @@
+window.addEventListener("scroll", () => {
+    var header = document.querySelector('header');
+    
+    if (window.scrollY > 0) {
+        header.classList.add("scrolled");
+    } else{
+        header.classList.remove("scrolled");
+    }
+});
